8dd31170073fe32e9e9ace1c02914293e6ad67ea
Eugene Orlov <sonic050288@gmail.com>
2019-07-21 21:41:35 +0300

    Parse csv to DataFrame

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
+import csv
 import psycopg2
+import pandas as pd
 
 conn = psycopg2.connect(dbname = 'hsetest', user = 'hseuser',
-							password = 'hsepass', host = 'localhost')
+						password = 'hsepass', host = 'localhost')
 
 cursor = conn.cursor()
-cursor.execute("SELECT * FROM test")
-print(cursor.fetchone())
+data = pd.read_csv('test_csv.csv')
+
+titles = str(data.dtypes).lower().split()
+#print(titles)
+
+for id in range(len(titles)):
+	if titles[id] == 'object':
+		titles[id] = 'TEXT,'
+	elif 'int' in titles[id]:
+		titles[id] = 'INT,'
+	elif 'float' in titles[id]:
+		titles[id] = 'FLOAT,'
+
+titles = ' '.join(titles[:-2]).split(',')
+titles = titles[:-1]
+
+query = '''CREATE TABLE test_csv({0})'''.format(','.join(titles))
+cursor.execute(query)
+
+conn.commit()
+
+with open('test_csv.csv', 'r') as f:
+	next(f)
+	cursor.copy_from(f, 'test_csv', sep = ',')
+
+conn.commit()
