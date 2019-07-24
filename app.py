57d9e8bca109a1b535f001d3d51b7a3b8e05dbce
Eugene Orlov <sonic050288@gmail.com>
2019-07-24 22:08:28 +0300

    Add sqlalchemy

--- app.py
+++ app.py
@@ -1,34 +1,40 @@
 import csv
 import psycopg2
 import pandas as pd
+from sqlalchemy import create_engine, MetaData, Table, select
 
-conn = psycopg2.connect(dbname = 'hsetest', user = 'hseuser',
-						password = 'hsepass', host = 'localhost')
+engine = create_engine("postgresql+psycopg2://hseuser:hsepass@localhost/hsetest")
+conn = engine.connect()
 
-cursor = conn.cursor()
 data = pd.read_csv('test_csv.csv')
+print(MetaData(conn))
+data.to_sql('test_csv', con = engine, if_exists = 'append')
 
-titles = str(data.dtypes).lower().split()
-#print(titles)
 
-for id in range(len(titles)):
-	if titles[id] == 'object':
-		titles[id] = 'TEXT,'
-	elif 'int' in titles[id]:
-		titles[id] = 'INT,'
-	elif 'float' in titles[id]:
-		titles[id] = 'FLOAT,'
 
-titles = ' '.join(titles[:-2]).split(',')
-titles = titles[:-1]
 
-query = '''CREATE TABLE test_csv({0})'''.format(','.join(titles))
-cursor.execute(query)
 
-conn.commit()
+# titles = str(data.dtypes).lower().split()
+# #print(titles)
 
-with open('test_csv.csv', 'r') as f:
-	next(f)
-	cursor.copy_from(f, 'test_csv', sep = ',')
+# for id in range(len(titles)):
+# 	if titles[id] == 'object':
+# 		titles[id] = 'TEXT,'
+# 	elif 'int' in titles[id]:
+# 		titles[id] = 'INT,'
+# 	elif 'float' in titles[id]:
+# 		titles[id] = 'FLOAT,'
 
-conn.commit()
+# titles = ' '.join(titles[:-2]).split(',')
+# titles = titles[:-1]
+
+# query = '''CREATE TABLE test_csv({0})'''.format(','.join(titles))
+# cursor.execute(query)
+
+# conn.commit()
+
+# with open('test_csv.csv', 'r') as f:
+# 	next(f)
+# 	cursor.copy_from(f, 'test_csv', sep = ',')
+
+# conn.commit()
