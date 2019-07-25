be109dc20a0acc595a2ece09477dd6ea4e90531a
Eugene Orlov <sonic050288@gmail.com>
2019-07-25 21:43:34 +0300

    Create API

--- app.py
+++ app.py
@@ -3,17 +3,24 @@ import db
 
 app = Flask(__name__)
 
+# путь формы для загрузки файла
 @app.route("/")
 def hello():
     return render_template('index.html')
 
+# путь парсинга csv и отправки данных в базу данных
 @app.route("/upload", methods = ['POST'])
 def upload():
-	file = request.files
-	print(file)
-	# file_name, data = db.parse_file(file)
-	# return file_name
+	# получаю файл из request
+	file = request.files['file']
 
+	# получаю имя файла и данные в виде DataFrame
+	file_name, data = db.parse_file(file)
+
+	# создание таблицы в базе данных и внесение данных в таблицу
+	db.create_table(file_name, data)
+
+	return "Ok"
 
 if __name__ == "__main__":
     app.run(debug = True)
--- db.py
+++ db.py
@@ -4,38 +4,13 @@ import pandas as pd
 from sqlalchemy import create_engine, MetaData, Table, select
 
 def parse_file(file):
-	file_name = file.filename
+	file_name = file.filename.split('.')[0]
 	data = pd.read_csv(file)
 	return file_name, data
 
+def create_table(table_name, data):
+	engine = create_engine("postgresql+psycopg2://hseuser:hsepass@localhost/hsetest")
+	conn = engine.connect()
+	data.to_sql(table_name, con = engine, if_exists = 'append')
 
 
-engine = create_engine("postgresql+psycopg2://hseuser:hsepass@localhost/hsetest")
-conn = engine.connect()
-# data.to_sql('test_csv', con = engine, if_exists = 'append')
-
-
-# titles = str(data.dtypes).lower().split()
-# #print(titles)
-
-# for id in range(len(titles)):
-# 	if titles[id] == 'object':
-# 		titles[id] = 'TEXT,'
-# 	elif 'int' in titles[id]:
-# 		titles[id] = 'INT,'
-# 	elif 'float' in titles[id]:
-# 		titles[id] = 'FLOAT,'
-
-# titles = ' '.join(titles[:-2]).split(',')
-# titles = titles[:-1]
-
-# query = '''CREATE TABLE test_csv({0})'''.format(','.join(titles))
-# cursor.execute(query)
-
-# conn.commit()
-
-# with open('test_csv.csv', 'r') as f:
-# 	next(f)
-# 	cursor.copy_from(f, 'test_csv', sep = ',')
-
-# conn.commit()
